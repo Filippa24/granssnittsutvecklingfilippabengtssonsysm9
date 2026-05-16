2ca00e4fa094d324a8222a0be2e8eb3f68f9cf15
Filippa24 <filippa98b@hotmail.se>
2026-05-16 10:08:35 +0200

    hero adjustments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 function Hero() {
     return (
       <section className="hero">
-        <div className="hero__text">
+        <div className="hero__text-container">
           <h1 className="hero__title">Company name</h1>
+          <h3 className="hero__mobile-text">Create user</h3>
+          <h3 className="hero__mobile-text">Sign in</h3>
           <h3 className="hero__slogan">Slogan</h3>
         </div>
       </section>
