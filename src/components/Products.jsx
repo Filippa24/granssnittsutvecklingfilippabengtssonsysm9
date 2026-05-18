62c5c2db8e95f23e8dfa4210ae162b76d65a7384
Filippa24 <filippa98b@hotmail.se>
2026-05-18 16:29:51 +0200

    filter feature

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
 import UseFetch from "../hooks/useFetch";
 import ProductCard from "./cards/ProductCard";
+import FilterCard from "./cards/FilterCard";
 
 function Products() {
   //variabel för url
   const url = "http://localhost:3001/motorcycles";
-
-  //hämta valfri data från db.json via usefetch-hook, döper variabeln data till products ist
+   //hämta valfri data från db.json via usefetch-hook, döper variabeln data till products ist
   const { data: products, loading, error } = UseFetch(url);
+
+  //filtrera produkter baserat på make (filter drop down)------------------------------------------------------
+  //variabel för vilket li man väljer i filter drop down
+  const [selectedMake, setSelectedMake] = useState(null);
+
+  //om selectedMake är true visas de filtrerade produkterna annars visas alla produkter. värdena sparas i filteredProducts
+  const filteredProducts = selectedMake ? products.filter(p => p.make === selectedMake) : products;
+//-------------------------------------------------------------------------------------------------------------
 
   console.log("products:", products);
   console.log("loading:", loading);
@@ -22,20 +30,23 @@ function Products() {
     );
   }
 
-  if (error) {
-    return <p>Error loading products.</p>;
-  }
-
-  if (!products) {
-    return <p>Products could not be found.</p>;
-  }
+  if (error) return <p>Error loading products.</p>;
+  if (!products) return <p>Products could not be found.</p>;
 
   return (
     <div className="products__background">
-      <div className="product__list">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} /> //skicka hela product-objektet i en variabel som heter product
-        ))}
+      <div className="products__layout">
+        <div className="product__list">
+          {/* mappar filteredproducts ist för bara products eftersom filteredproducts kan returnera både filtrerade produkter eller produkter beroende på situation, products kunde bara returnera alla products utan filter*/}
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} /> //skicka hela product-objektet i en variabel som heter product
+          ))}
+        </div>
+        {/* när man väljer li i filtret (onSelectMake), sätts setSelectedMake. detta görs via filtercard */}
+        <FilterCard
+          onSelectMake={setSelectedMake}
+          selectedMake={selectedMake}
+        />
       </div>
     </div>
   );
---
+++ src/components/cards/FilterCard.jsx
@@ -0,0 +1,59 @@
+import UseFetch from "../../hooks/useFetch";
+import { useState } from "react";
+
+function FilterCard({ onSelectMake, selectedMake }) {
+  const [showFilter, setShowFilter] = useState(false);
+
+  //variabel för url
+  const url = "http://localhost:3001/motorcycles";
+
+  const { data: products, loading, error } = UseFetch(url);
+
+  if (loading) return <p>Loading filter.</p>;
+  if (error) return <p>Error loading filter.</p>;
+  if (!products) return <p>Filter could not be found.</p>;
+
+  return (
+    <div className="filter__container">
+      <button
+        onClick={() => setShowFilter(!showFilter)}
+        className="filter__btn"
+        style={{ visibility: showFilter ? "hidden" : "visible" }}
+      >
+        Filter
+      </button>
+      {/* visa filter och sortera i alfabetisk ordning */}
+      {showFilter && (
+        <ul className="filter__list">
+          {/* visa alla produkter: */}
+          <li
+            className="filter__li"
+            onClick={() => {
+                onSelectMake(null);
+            setShowFilter(false);
+        }} // null = visa alla
+          >
+            Show all
+          </li>
+
+          {/* plocka ut bara make via map, make läggs i en ny array som direkt skickas in i new set där dubletter tas bort, sen sprids de ut i en ny array, sorteras och skrivs ut */}
+          {/* 1. [... new set] sprider ut setvärdena i en ny array. 2. new set tar bort dubletter, ett set kan bara innehålla unika värden. 3. plocka ut bara make via map 4. sort() sorterar alfabetiskt. 5. map() renderar varje make osm en li*/}
+          {[...new Set(products.map((p) => p.make))].sort().map((make) => (
+            <li
+              key={make}
+              className="filter__li"
+              onClick={() => {
+                onSelectMake(make); 
+                setShowFilter(false);
+            }}
+            >
+              {make}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default FilterCard;
